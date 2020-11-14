60d47f5a81683bead760fccd23e51d056183f4ca
gaaalmeida <gaaalmeidasan@gmail.com>
2020-11-14 02:45:55 -0300

    rank, prefeitos & tabs

--- js/main.js
+++ js/main.js
@@ -18,4 +18,43 @@ class Toggle {
 
 
 // * Mobile navbar toggle
-new Toggle("sidebar", "navbar-toggler" ,"show");
+new Toggle("sidebar", "navbar-toggler", "show");
+
+//Candidatos
+new Toggle("profilediv", "candidato-1-m", "d-none");
+new Toggle("black", "candidato-1-m", "d-none");
+new Toggle("profilediv", "candidato-1-m-c", "d-none");
+new Toggle("black", "candidato-1-m-c", "d-none");
+
+
+new Toggle("profilediv", "black", "d-none");
+new Toggle("black", "black", "d-none");
+
+// Melhores/Piores prefeitos
+const melhoresPrefeitos = () => {
+    const melhores = document.querySelector("#ranking-melhores-prefeitos");
+    const piores = document.querySelector("#ranking-piores-prefeitos");
+    const mB = document.querySelector("#melhoresPrefeitos");
+    const pB = document.querySelector("#pioresPrefeitos");
+
+    mB.classList.add("btn-primary");
+    pB.classList.remove("btn-primary");
+    melhores.classList.remove("d-none");
+    piores.classList.add("d-none");
+}
+const pioresPrefeitos = () => {
+    const melhores = document.querySelector("#ranking-melhores-prefeitos");
+    const piores = document.querySelector("#ranking-piores-prefeitos");
+    const mB = document.querySelector("#melhoresPrefeitos");
+    const pB = document.querySelector("#pioresPrefeitos");
+
+    pB.classList.add("btn-primary");
+    mB.classList.remove("btn-primary");
+    melhores.classList.add("d-none");
+    piores.classList.remove("d-none");
+}
+
+$('#profiletab a').on('click', function (e) {
+    e.preventDefault();
+    $(this).tab('show');
+})
